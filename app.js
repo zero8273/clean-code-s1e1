3d33df63a94e4b236f706c78e4d758c377748377
Sergeii Samus <cergi8273@gmail.com>
2023-04-24 17:30:08 +0300

    refactor(basic-3.4):Edited HTML CSS JS replacing all tag selectors

--- app.js
+++ app.js
@@ -33,18 +33,19 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
-
+    label.className='task task-lable';
+    listItem.className ='task-list__item'
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
     editInput.className="task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="edit btn";
 
-    deleteButton.className="delete";
+    deleteButton.className="delete btn";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className='btn-icon'
     deleteButton.appendChild(deleteButtonImg);
 
 
